83680da3fbd164a992f84ab3958a3a61149b3b28
W1lly2 <167796456+W1lly2@users.noreply.github.com>
2025-07-03 12:16:01 -0600

    Delete backend directory

--- backend/database.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = "postgresql+asyncpg://postgres:c4rec4@147.182.190.223/gtim_services"
-
-engine = create_async_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
-Base = declarative_base()
